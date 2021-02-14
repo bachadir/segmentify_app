eb4a581be8f0b45ada3f42b42a81c997be26d86e
bachadir <bahadirtasdelen@gmail.com>
2021-02-14 15:11:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-    "log"
-
-    "github.com/gofiber/fiber/v2"
+    "fmt"
+    "net/http"
 )
 
+func helloWorld(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello World")
+}
+
 func main() {
-    // Create new Fiber instance
-    app := fiber.New()
-
-    // Make path with some content
-    app.Get("/hello", func(c *fiber.Ctx) error {
-        // Return a string with a dummy text
-        return c.SendString("Hello, World 👋!")
-    })
-
-    // Start server on http://localhost:3000 or error
-    log.Fatal(app.Listen(":3000"))
+    http.HandleFunc("/", helloWorld)
+    http.ListenAndServe(":80", nil)
 }
